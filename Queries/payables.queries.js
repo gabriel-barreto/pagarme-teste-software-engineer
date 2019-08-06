e287e333e423d16e218a04e35ac3170f63f8740e
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-08-06 04:13:49 -0300

    Work with transactions to improve statement

--- Queries/payables.queries.js
+++ Queries/payables.queries.js
@@ -3,7 +3,10 @@ import { Payable } from '../Models';
 const create = payload => new Payable(payload).save();
 
 const getStatement = sinceDate =>
-    Payable.find({ paymentDate: { $gte: sinceDate } });
+    Payable.find({ paymentDate: { $gte: sinceDate } }).populate(
+        'transaction',
+        'description date',
+    );
 
 const getBalance = () =>
     Payable.aggregate([
