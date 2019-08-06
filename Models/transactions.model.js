fffe713590bc1161cdf53eb6b6ed2af47a84b630
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-08-06 04:13:35 -0300

    Fix schema, add schema middlewares

--- Models/transactions.model.js
+++ Models/transactions.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 
+import { payables } from '../Functions/Models';
+
 const cardNumbersValidator = v => {
     return !`${v}`
         .split('')
@@ -7,7 +9,7 @@ const cardNumbersValidator = v => {
         .includes(true);
 };
 
-const transactionSchema = {
+const transactionSchema = new Schema({
     value: {
         type: Number,
         required: true,
@@ -48,7 +50,55 @@ const transactionSchema = {
             validate: cardNumbersValidator,
         },
     },
-};
+    date: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+});
+
+transactionSchema.pre('save', function(doc) {
+    doc.date = new Date();
+    return doc;
+});
+
+transactionSchema.post('save', function(doc) {
+    const getPaymentDate = paymentMethod => {
+        if (paymentMethod == 'debit_card') return doc.date.setHours(0, 0, 0, 0);
+        return new Date(doc.date.setDate(doc.date.getDate() + 30)).setHours(
+            0,
+            0,
+            0,
+            0,
+        );
+    };
+
+    const getStatus = paymentMethod => {
+        if (paymentMethod == 'debit_card') return 'paid';
+        return 'waiting_funds';
+    };
+
+    const genPaymentInfo = paymentMethod => ({
+        status: getStatus(paymentMethod),
+        paymentDate: getPaymentDate(paymentMethod),
+    });
+
+    const payload = {
+        value: doc.value,
+        transaction: doc._id,
+        ...genPaymentInfo(doc.paymentMethod),
+
+        // Worse than wrapper
+        // paymentDate: getPaymentDate(doc.paymentMethod),
+        // status: getStatus(doc.paymentMethod),
+
+        // Barely readable
+        // status: doc.paymentMethod == "debit_card" ? "paid" : "waiting_funds",
+        // paymentDate: doc.paymentMethod == "debit_card" ? new Date() : new Date().setDate(new Date().getDate() + 30),
+    };
+
+    return payables.create(payload);
+});
 
 const model = mongoose.model('Transaction', transactionSchema, 'transactions');
 
