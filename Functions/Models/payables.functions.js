999d5cf90a66907aaded311d6c6fd533c73483fe
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-08-06 04:12:57 -0300

    Add handler to when no payable is found

--- Functions/Models/payables.functions.js
+++ Functions/Models/payables.functions.js
@@ -1,4 +1,5 @@
 import { payables } from '../../Queries';
+import { parseCIDR } from 'ipaddr.js';
 
 const create = payload => payables.create(payload);
 
@@ -13,11 +14,26 @@ const getStatement = rawSinceDate => {
 };
 
 const getBalance = () =>
-    payables.getBalance().then(docs =>
-        docs.map(each => ({
-            status: each._id,
-            value: each.value,
-        })),
-    );
+    new Promise((resolve, reject) => {
+        const defaultBalance = [
+            { status: 'waiting_funds', value: 0 },
+            { status: 'paid', value: 0 },
+        ];
+
+        payables
+            .getBalance()
+            .then(docs => {
+                if (!docs || docs.length < 1) return resolve(defaultBalance);
+                return docs;
+            })
+            .then(entries => {
+                return entries.map(each => ({
+                    status: each._id,
+                    value: each.value,
+                }));
+            })
+            .then(resolve)
+            .catch(reject);
+    });
 
 export default { create, getBalance, getStatement };
